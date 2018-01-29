e34327734187754e98daf8149f32e589fd983816
Bryan Wilkinson <bwilk1@umbc.edu>
2018-01-29 14:58:36 -0500

    Setting kernel enviromental variables programatically using python

---
+++ binder/set_envs.py
@@ -0,0 +1,17 @@
+#!/bin/python
+import sys
+import json
+
+kernel_path = sys.argv[1]
+variables_path = sys.argv[2]
+
+kernel = json.load(open(kernel_path))
+env = {}
+
+with open(variables_path) as f:
+	for line in f:
+		parts = line.strip().split(":")
+		env[part[0]] = part[1]
+
+kernel['env'] = env
+json.dump(kernel,open(kernel_path,'w'))
